1b78e7faf656760009c59500bc380c4ab8386289
RoxasAPark <53633811+RoxasAPark@users.noreply.github.com>
2021-10-26 13:24:57 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,35 +1,30 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ChainingHashTable
+namespace OpenAddressingHashTable
 {
     class Program
-    {    
-
-        // Class representing Employees as objects to enter into the hash table
+    {
+        // Class to define an employee object to insert into the hash table
         class Employee
         {
             private string firstName;
             private string lastName;
-            private int id;
 
-            // Since separate chaining requires a linked list, there needs to be a pointer
-            // to the next employee
-            public Employee next;
+            // Each employee is assigned an integer as ID prior to insertion to the table
+            private int ID;
 
-            // Constructor 
-            public Employee(string f, string l, int i)
+            // Constructor for the employee object
+            public Employee(string fName, string lName, int idNum)
             {
-                this.firstName = f;
-                this.lastName = l;
-                this.id = i;
-                next = null;
+                this.firstName = fName;
+                this.lastName = lName;
+                this.ID = idNum;
             }
 
-            // Functions to allow users to see information about each employee in the hash table
             public string getFirstName()
             {
                 return this.firstName;
@@ -42,90 +37,110 @@ namespace ChainingHashTable
 
             public int getID()
             {
-                return this.id;
+                return this.ID;
             }
         }
 
-
-        // Hash table class
+        // Class for the hash table equipped with open addressing functions
         class HashTable
         {
-            // The number of indices for the table to contain
+            // Number of indices for the table
             private int indices;
-            private Employee [] table;
+            private Employee[] table;
+
+            // An integer to keep count of Employee objects inserted into the table
+            private int count;
 
             // Constructor to initialize the hash table
             public HashTable(int i)
             {
                 indices = i;
                 table = new Employee[indices];
+                count = 0;
             }
 
-            // Hash function to assign each Employee an index based on the key value
-            public int hashFunction(int ky)
+            // A function to check if there are no more empty indices left
+            public bool isFull()
             {
-                return ky % indices;
+                return (count == table.Length);
             }
 
-            // Function to insert an employee into the hash table
-            public void insert(string firstNm, string lastNm, int id)
+            // Hash function to assign an employee an index
+            public int hashFunction(int key)
             {
+                return (key % indices);
+            }
 
-                // Assign an employee to insert an index
-                int index = hashFunction(id);
+            // Function to insert an employee to the hash table
+            public void insert(string first, string last, int k)
+            {
+                // Get the index to assign an employee
+                int index = hashFunction(k);
 
-                // If the index is empty, just perform a simple insertion
-                if (table[index] == null)
-                    table[index] = new Employee(firstNm, lastNm, id);
-                else // Otherwise perform the chaining function if the index is occupied
+                if (isFull())
+                    Console.WriteLine("The table is full.");
+                else
                 {
-                    
-                    Employee current = table[index];
+                    // Insert into the index if empty
+                    if (table[index] == null)
+                    {
+                        table[index] = new Employee(first, last, k);
+                    }
+                    else // Find the next empty index
+                    {
+                        int currentIndex = index;
+                        while (table[currentIndex] != null)
+                        {
+                            if (currentIndex == table.Length - 1)
+                                currentIndex = 0;
 
-                    // Traverse to the end of the linked list at the assigned index
-                    while (current.next != null)
-                        current = current.next;
+                            currentIndex++;
+                        }
 
-                    // Insertion via chaining
-                    current.next = new Employee(firstNm, lastNm, id);
-                }
+                        // Insert into the empty index if found
+                        table[currentIndex] = new Employee(first, last, k);
+                    }
+
+                    // Update the count
+                    count++;
+                }            
             }
 
-            // Function to remove an employee based on the key value
-            public void delete(int key)
+            // Function to remove an employee from the table
+            public void delete(int k)
             {
-                // Locate the index where the key may be located
-                int index = hashFunction(key);
-
-                Employee current = table[index];
-
-                // If the index is empty, do not perform the deletion
-                if (current == null)
+                // An index cannot be negative
+                if (k < 0)
                     return;
                 else
                 {
-                    // Find the key inside the linked list at 
-                    bool keyFound = false;
+                    // Locate the index where the employee may be
+                    int index = hashFunction(k);
 
-                    while (current.next != null)
-                    {
-                        if(current.next.getID() != key)
-                            current = current.next;
-                        else // if the key is found, stop looping
-                        {
-                            keyFound = true;
-                            break;
-                        }
-                    }
-
-                    // Perform the deletion only if the key is found
-                    if (keyFound == true)
-                    {
-                        Employee temp = current.next.next;
-                        current.next = temp;
-                    }
+                    // If the employee is found, remove it immediately
+                    if (table[index].getID() == k)
+                        table[index] = null;
                     else
-                        Console.WriteLine("An employee with the ID: " + key + " does not exist in the table");
+                    {
+                        // Otherwise, search every other index until the employee is found
+                        bool keyFound = false;
+                        int current;
+
+                        for(current = 0; current < table.Length;current++)
+                        {
+                            if(table[current].getID() == k)
+                            {
+                                keyFound = true;
+                                break;
+                            }
+                        }
+
+                        if (keyFound == true)
+                            table[index] = null;
+                        else
+                            Console.WriteLine("An employee with that ID number doesn't exist");
+
+                    }
                 }
             }
 
@@ -134,30 +149,25 @@ namespace ChainingHashTable
             {
                 for(int i = 0; i < table.Length; i++)
                 {
-                    if (table[i] == null)
-                        Console.WriteLine(i + ":  " + "No entry");
-                    else
-                    {
-                        for (Employee current = table[i]; current != null; current = current.next)
-                        {
-                            Console.WriteLine(i + ":  " + current.getFirstName() + " "
-                                + current.getLastName() + " " + current.getID());
-                        }
-                    }
+                    if (table[i] != null)
+                        Console.WriteLine(i + ": " + table[i].getFirstName() + " " + table[i].getLastName()
+                            + " " + table[i].getID());
                 }
             }
         }
 
         static void Main(string[] args)
         {
-            HashTable ht = new HashTable(10);
+            HashTable ht = new HashTable(6);
 
             ht.insert("Priscilla", "Betti", 19);
-            ht.insert("Lorie", "Pester", 16);
-            ht.insert("Magnus", "Carlsen", 21);
-            ht.insert("Tifa", "Lockhart", 11);
-            ht.insert("Julie", "Zenatti", 18);
-            ht.insert("Hikaru", "Nakamura", 9);
+            ht.insert("Hikaru", "Nakamura", 16);
+            ht.insert("Lorie", "Pester", 18);
+            ht.insert("Magnus", "Carlsen", 11);
+            ht.insert("Falco", "Lombardi", 41);
+            ht.insert("Tifa", "Lockhart", 48);
+
+            ht.insert("Eddie", "Pulaski", 10);
 
             ht.delete(11);
 
